f5baa8c78347b83018f3cddeb1ef20b5d9a0acc4
ahmadehsas <ahmadehsas14345@gmail.com>
2025-12-01 01:54:52 +0000

    Clearing the root element and looping.

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+  // Get the root container
+  // select the < dive id = "root"> in HTML
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // clear any text or previous content inside the root.
+  rootElem.innerHTML = "";
+
+  // loop over all episodes
+  // currently it just logs each episode to the console so we can inspect the data
+  episodeList.forEach((episode) => {
+    console.log(episode); 
+  })
+   
 }
 
 window.onload = setup;
